48c62ffe53ea1eb0367c18aea3d64cbb4fae84d8
Alauskas <37072295+Alauskas@users.noreply.github.com>
2020-04-29 20:25:53 +0300

    development

--- Training system/Training Management/Training system/TMS/Utils/Settings.cs
+++ Training system/Training Management/Training system/TMS/Utils/Settings.cs
@@ -10,9 +10,7 @@ namespace TMS
     {
             public static Guid ProjectId { get; set; } = Guid.Parse("813cac02-a70f-48c7-899c-67fc84676b64");
             public static string ApiKey { get; set; } = "KC6GsNe9v7g3U1jQPIqHCOfWWyplQB6A";
-
-        //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
-            public static string SendGridApiKey { get; set; } = "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";
+            public static string SendGridApiKey { get; set; } = Environment.GetEnvironmentVariable("ApiKey");
             public static string EmailConfirmationTemplateId { get; set; } = "d-12425b91466e46149de263bc746efef4";
             public static string PasswordResetEmailTemplateId { get; set; } = "d-ebcd2abece194f8ca0bfce98b822e5f2";
             public static string PaswordSendTemplate { get; set; } = "d-d63068bab910495ea6b93191b14e45f1";
