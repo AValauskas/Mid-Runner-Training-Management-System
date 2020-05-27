3f128a7e0633853f0e97d09d1f7e8e126ec76b88
Alauskas <37072295+Alauskas@users.noreply.github.com>
2020-05-27 23:21:53 +0300

    tikrinu

--- TrainingSystem/TrainingManagement/TrainingSystem/TMS/Utils/Settings.cs
+++ TrainingSystem/TrainingManagement/TrainingSystem/TMS/Utils/Settings.cs
@@ -10,7 +10,7 @@ namespace TMS
     {
         public static Guid ProjectId { get; set; } = Guid.Parse("813cac02-a70f-48c7-899c-67fc84676b64");
         public static string ApiKey { get; set; } = "KC6GsNe9v7g3U1jQPIqHCOfWWyplQB6A";
-        public static string SendGridApiKey { get; set; } = Environment.GetEnvironmentVariable("ApiKey");
+        public static string SendGridApiKey { get; set; } = "a";//Environment.GetEnvironmentVariable("ApiKey");
         public static string EmailConfirmationTemplateId { get; set; } = "d-12425b91466e46149de263bc746efef4";
         public static string PasswordResetEmailTemplateId { get; set; } = "d-ebcd2abece194f8ca0bfce98b822e5f2";
         public static string PaswordSendTemplate { get; set; } = "d-d63068bab910495ea6b93191b14e45f1";
